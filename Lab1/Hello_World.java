da26b9fd6048c644da398402a21b75f4c96c3314
phandohaiminh <85116583+phandohaiminh@users.noreply.github.com>
2021-05-31 21:57:44 +0700

    Add files via upload

---
+++ Lab1/Hello_World.java
@@ -0,0 +1,10 @@
+public class helloWorld {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Xin chao \n cac ban !");
+        System.out.print("Hello \t world ! ");
+       
+	}
+
+}
